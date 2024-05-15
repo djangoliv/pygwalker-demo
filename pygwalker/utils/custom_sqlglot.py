063d6f9f6569871802f298ceddab53937cce88e1
Olivier Giorgis <ogiorgis@logilab.fr>
2024-05-15 11:19:59 +0200

    clean

--- pygwalker/utils/custom_sqlglot.py
+++
@@ -1,7 +0,0 @@
-from sqlglot.dialects.duckdb import DuckDB as DuckdbDialect
-from sqlglot import exp
-from sqlglot.helper import seq_get
-
-DuckdbDialect.Parser.FUNCTIONS["EPOCH_MS"] = lambda args: exp.UnixToTime(
-    this=exp.Div(this=seq_get(args, 0), expression=exp.Literal.number(1000.0))
-)
